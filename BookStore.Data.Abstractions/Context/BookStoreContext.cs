d9a161d4ae437c4cb6c10c7fb46bb4b0a0bb9401
thiseasm <44376778+thiseasm@users.noreply.github.com>
2025-02-01 00:29:11 +0200

    Add relationships between dtos

--- BookStore.Data.Abstractions/Context/BookStoreContext.cs
+++ BookStore.Data.Abstractions/Context/BookStoreContext.cs
@@ -1,6 +1,26 @@
-﻿namespace BookStore.Data.Abstractions.Context
+﻿using BookStore.Data.Abstractions.DTOs;
+using BookStore.Data.Abstractions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data.Abstractions.Context
 {
-    public class BookStoreContext
+    public class BookStoreContext : DbContext
     {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDto>()
+                .HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .UsingEntity<UserRoleDto>();
+
+            modelBuilder.Entity<BookDto>()
+                .HasOne(e => e.Category)
+                .WithMany()
+                .HasForeignKey(e => e.CategoryId)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ BookStore.Data.Abstractions/DTOs/UserRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Data.Abstractions.DTOs
+{
+    public class UserRoleDto
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
