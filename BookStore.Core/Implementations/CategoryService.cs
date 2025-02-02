71470e12450361f15264a58c63617abdca92a95d
thiseasm <44376778+thiseasm@users.noreply.github.com>
2025-02-02 20:57:33 +0200

    Implement error handling in Category and Role Services

--- BookStore.Core/Implementations/CategoryService.cs
+++ BookStore.Core/Implementations/CategoryService.cs
@@ -4,48 +4,65 @@ using BookStore.Core.Abstractions.Models.ApiResponses;
 using BookStore.Core.Extensions;
 using BookStore.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BookStore.Core.Implementations
 {
-    public class CategoryService(BookStoreContext dbContext) : ICategoryService
+    public class CategoryService(ILogger<CategoryService> logger, BookStoreContext dbContext) : ICategoryService
     {
         public async Task<ApiResponse<string>> DeleteCategoryAsync(int categoryId, CancellationToken cancellationToken)
         {
-            var category = await dbContext.Categories
-                .Where(category => category.Id == categoryId && category.IsActive)
-                .FirstOrDefaultAsync(cancellationToken);
-
-            if (category is null)
+            try
             {
-                return ApiResponse<string>.NotFound($"Category with ID: {categoryId} not found");
+                var category = await dbContext.Categories
+                    .Where(category => category.Id == categoryId && category.IsActive)
+                    .FirstOrDefaultAsync(cancellationToken);
+
+                if (category is null)
+                {
+                    return ApiResponse<string>.NotFound($"Category with ID: {categoryId} not found");
+                }
+
+                var booksThatUseCategory = await dbContext.Books
+                    .Where(book => book.CategoryId == categoryId)
+                    .AsNoTracking()
+                    .CountAsync(cancellationToken);
+
+                if (booksThatUseCategory > 0)
+                {
+                    return ApiResponse<string>.Conflict($"Category with ID: {categoryId} is in use by {booksThatUseCategory} Books");
+                };
+
+                category.IsActive = false;
+                await dbContext.SaveChangesAsync(cancellationToken);
+
+                return ApiResponse<string>.Ok("Category deleted successfully");
             }
-
-            var booksThatUseCategory = await dbContext.Books
-                .Where(book => book.CategoryId == categoryId)
-                .AsNoTracking()
-                .CountAsync(cancellationToken);
-
-            if (booksThatUseCategory > 0)
+            catch (Exception ex)
             {
-                return ApiResponse<string>.Conflict($"Category is in use by {booksThatUseCategory} books");
-            };
-
-            category.IsActive = false;
-            await dbContext.SaveChangesAsync(cancellationToken);
-
-            return ApiResponse<string>.Ok("Category deleted successfully");
-
+                logger.LogError(ex, "Unexpected error while deleting Category with ID: {category}", categoryId);
+                return ApiResponse<string>.InternalError("An unexpected error occurred.");
+            }
         }
 
         public async Task<ApiResponse<IList<Category>>> GetCategoriesAsync(CancellationToken cancellationToken)
         {
-            var result = await dbContext.Categories
+            try
+            {
+                var result = await dbContext.Categories
                 .Where(category => category.IsActive)
                 .AsNoTracking()
                 .Select(category => category.ToDomain())
                 .ToListAsync(cancellationToken);
 
-            return ApiResponse<IList<Category>>.Ok(result);
+                return ApiResponse<IList<Category>>.Ok(result);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unexpected error while retrieving Categories");
+                return ApiResponse<IList<Category>>.InternalError("An unexpected error occurred.");
+            }
+
         }
     }
 }
--- BookStore.Core/Implementations/RoleService.cs
+++ BookStore.Core/Implementations/RoleService.cs
@@ -3,24 +3,33 @@ using BookStore.Core.Abstractions.Models;
 using BookStore.Core.Abstractions.Models.ApiResponses;
 using BookStore.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BookStore.Core.Implementations
 {
-    public class RoleService(BookStoreContext dbContext) : IRoleService
+    public class RoleService(ILogger<RoleService> logger, BookStoreContext dbContext) : IRoleService
     {
         public async Task<ApiResponse<IList<Role>>> GetRolesAsync(CancellationToken cancellationToken)
         {
-            var result = await dbContext.Roles
-                .OrderBy(role => role.Id)
-                .AsNoTracking()
-                .Select(role => new Role
-                {
-                    Id = role.Id,
-                    Name = role.Name
-                })
-                .ToListAsync(cancellationToken);
+            try
+            {
+                var result = await dbContext.Roles
+                    .OrderBy(role => role.Id)
+                    .AsNoTracking()
+                    .Select(role => new Role
+                    {
+                        Id = role.Id,
+                        Name = role.Name
+                    })
+                    .ToListAsync(cancellationToken);
 
-            return ApiResponse<IList<Role>>.Ok(result);
+                return ApiResponse<IList<Role>>.Ok(result);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unexpected error while retrieving Roles");
+                return ApiResponse<IList<Role>>.InternalError("An unexpected error occurred.");
+            }           
         }
     }
 }
