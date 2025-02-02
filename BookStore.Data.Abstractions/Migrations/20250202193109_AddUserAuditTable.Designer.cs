2e859a3cf1b014963fb735c4da28af19f5b022f8
thiseasm <44376778+thiseasm@users.noreply.github.com>
2025-02-02 21:31:34 +0200

    Add audit table migration

--- BookStore.Data.Abstractions/Migrations/20250202193109_AddUserAuditTable.Designer.cs
+++ BookStore.Data.Abstractions/Migrations/20250202193109_AddUserAuditTable.Designer.cs
@@ -4,6 +4,7 @@ using BookStore.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookStore.Data.Abstractions.Migrations
 {
     [DbContext(typeof(BookStoreContext))]
-    partial class BookStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20250202193109_AddUserAuditTable")]
+    partial class AddUserAuditTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookStore.Data.Abstractions/Migrations/20250202193109_AddUserAuditTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Data.Abstractions.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAuditTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLogs",
+                columns: table => new
+                {
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EntityId = table.Column<int>(type: "int", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PreviousState = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NextState = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLogs", x => new { x.Timestamp, x.EntityId });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserLogs");
+        }
+    }
+}
